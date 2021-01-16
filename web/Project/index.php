3fb55542e4242cc68cce2ba1ebe45dc362d05426
Kat Nelson <kat98058@byui.edu>
2021-01-15 21:15:38 -0600

    creating homepage for project

---
+++ web/Project/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Family Recipes</title>
+    <script src="js/main.js"></script>
+    <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <?php include $_SERVER['DOCUMENT_ROOT'].'nav.php'; ?>
+            </nav>
+        </header>
+    </body>
+</html>
