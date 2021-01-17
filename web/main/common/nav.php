be0b41a476d8e93ca4f9b4ca97c053c368ad2ea2
Kat Nelson <kat98058@byui.edu>
2021-01-16 20:47:01 -0600

    updates

--- web/main/common/nav.php
+++ web/main/common/nav.php
@@ -1,6 +1,6 @@
 <ul class = "menu">
                     <li class = "menu_item"><a href="">Home</a></li>
-                    <li class = "menu_item"><a href="">About Me</a></li>
+                    <li class = "menu_item"><a href="./views/aboutMe.php">About Me</a></li>
                     <li class = "menu_item"><a href="./views/assignments.php">Class Work</a></li>
            
                 </ul>
