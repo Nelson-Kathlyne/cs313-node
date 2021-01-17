06eb6c311510256378c2fc620cccc9bc15187bcf
Kat Nelson <kat98058@byui.edu>
2021-01-16 20:18:32 -0600

    trying to add through git bash

--- web/main/common/nav.php
+++ web/main/common/nav.php
@@ -1,6 +1,6 @@
 <ul class = "menu">
                     <li class = "menu_item"><a href="">Home</a></li>
                     <li class = "menu_item"><a href="">About Me</a></li>
-                    <li class = "menu_item"><a href="">Class Work</a></li>
+                    <li class = "menu_item"><a href="./views/assignments.php">Class Work</a></li>
            
                 </ul>
