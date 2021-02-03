5171e6a6c2027cd5f721a1ff06eb2a0be7959eab
bourn404 <bourn404@gmail.com>
2021-02-02 20:44:49 -0700

    a

--- web/index.php
+++ web/index.php
@@ -2,6 +2,12 @@
 
 require 'db.php';
 
+
+var_dump($_GET);
+
+
+
+
 $connection = connectDB();
 
 $sql = "SELECT * from scriptures";
@@ -24,12 +30,15 @@ $stmt->closeCursor();
   <title>Scriptures</title>
 </head>
 <body>
-  <form action="" method="get"></form>
+  <form action="/" method="get">
+    <label for="search"></label>
+    <input type="search" name="search" id="search">
+  </form>
   <h1>Scripture Resources</h1>
   <ul>
   <?php
     foreach($response as $scripture) {
-      echo "<li><strong>$scripture[book] $scripture[chapter]:$scripture[verse]</strong> - $scripture[content]</li>";
+      echo "<li><strong>$scripture[book] $scripture[chapter]:$scripture[verse]</strong> - \"$scripture[content]\"</li>";
     }
   ?>
   </ul>
