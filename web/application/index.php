c640810ce308194e2bff89a3b96ca14f50bd33c8
Kat Nelson <kat98058@byui.edu>
2021-02-08 21:18:37 -0600

    a

--- web/application/index.php
+++ web/application/index.php
@@ -1,35 +1,4 @@
-<?php
 
-require 'connection.php';
-
-$search = "";
-if(isset($_GET['search'])){
-  $search = filter_input(INPUT_GET,'search',FILTER_SANITIZE_STRING);
-}
-
-
-
-$connection = connectDB();
-
-
-$sql = "SELECT * from scriptures" ;
-
-var_dump($sql);
-
-$stmt = $connection->prepare($sql);
-
-$stmt->bindValue(':search', $search, PDO::PARAM_STR);
-$stmt->execute();
-$response = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$stmt->closeCursor();
-
-$message = "";
-if($stmt->rowCount() == 0) {
-  $message = "No results found.";
-}
-
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
