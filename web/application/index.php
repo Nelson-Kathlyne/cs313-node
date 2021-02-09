9c4922f41980b333a158939838d0980305e52fc5
Kat Nelson <kat98058@byui.edu>
2021-02-08 20:49:51 -0600

    a

--- web/application/index.php
+++ web/application/index.php
@@ -1,16 +1,59 @@
+<?php
+
+require 'connection.php';
+
+$search = "";
+if(isset($_GET['search'])){
+  $search = filter_input(INPUT_GET,'search',FILTER_SANITIZE_STRING);
+}
+
+$connection = connectDB();
+
+$sql = 'SELECT * from recipes';
+
+
+$sql = 'SELECT recipeName, recipes.foodIndexId FROM recipes INNER JOIN foodIndex
+ON foodIndex.foodIndexId = recipes.foodIndexId';
+//if($search) {
+  //$sql = "SELECT * from recipes WHERE book = :search";
+//}
+
+$stmt = $connection->prepare($sql);
+
+$stmt->bindValue(':search', $search, PDO::PARAM_STR);
+$stmt->execute();
+$response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt->closeCursor();
+
+$message = "";
+if($stmt->rowCount() == 0) {
+  $message = "No results found.";
+}
+
+
+?>
 
 <!DOCTYPE html>
-<html>
-    <head>
-        <title> Week 5</title>
-        <meta charset = "UTF-8">
-    </head>
-    <body>
-        <h1>Week 5</h1>
-        <ul>
-            <li><a href='notes.html'>Notes</a></li>
-            <li><a href=''>Code Examples</a></li>
-            <li><a href='teams.html'>Team Activities</a></li>
-        </ul>
-    </body>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Scriptures</title>
+</head>
+<body>
+  <form action="/" method="get">
+    <label for="search"></label>
+    <input type="search" name="search" id="search">
+    <input type="submit" value="search"/>
+  </form>
+  <h1>Recipes</h1>
+  <ul>
+  <?php
+    foreach($response as $scripture) {
+      echo "<li><a href='/scripture.php?id=$scripture[id]'><strong>$scripture[book] $scripture[chapter]:$scripture[verse]</strong></a></li>";
+    }
+  ?>
+  </ul>
+  <h4 style="color: red"><?php echo $message;?></h4>
+</body>
 </html>
