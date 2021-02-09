709fe4c1ff8794ece6b3853a49ab1f1329a19bda
Kat Nelson <kat98058@byui.edu>
2021-02-08 20:41:10 -0600

    a

--- web/application/index.php
+++ web/application/index.php
@@ -1,37 +1,4 @@
-<?php
 
-require 'connection.php';
-
-$search = "";
-if(isset($_GET['search'])){
-  $search = filter_input(INPUT_GET,'search',FILTER_SANITIZE_STRING);
-}
-
-$connection = connectDB();
-
-$sql = 'SELECT * from recipes';
-
-
-$sql = 'SELECT recipeName, recipes.foodIndexId FROM recipes INNER JOIN foodIndex
-ON foodIndex.foodIndexId = recipes.foodIndexId';
-//if($search) {
-  //$sql = "SELECT * from recipes WHERE book = :search";
-//}
-
-$stmt = $connection->prepare($sql);
-
-$stmt->bindValue(':search', $search, PDO::PARAM_STR);
-$stmt->execute();
-$response = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$stmt->closeCursor();
-
-$message = "";
-if($stmt->rowCount() == 0) {
-  $message = "No results found.";
-}
-
-
-?>
 <!DOCTYPE html>
 <html>
     <head>
