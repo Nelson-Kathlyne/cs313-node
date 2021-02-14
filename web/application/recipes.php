63b687898951e75c6b3fd4af638bcce5a059ba7c
Kat Nelson <kat98058@byui.edu>
2021-02-13 20:12:02 -0600

    a

--- web/application/recipes.php
+++ web/application/recipes.php
@@ -1,31 +1,4 @@
-<?php
 
-require 'db.php';
-
-$id = "";
-if(isset($_GET['id'])){
-  $id = filter_input(INPUT_GET,'id',FILTER_SANITIZE_NUMBER_INT);
-}
-
-$connection = connectDB();
-
-$message = "";
-if($id) {
-  $sql = "SELECT * from scriptures WHERE id = :id";
-
-  $stmt = $connection->prepare($sql);
-
-    $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-    $stmt->execute();
-    $scripture = $stmt->fetch(PDO::FETCH_ASSOC);
-    $stmt->closeCursor();
-} else {
-    $message = "No results found.";
-    die();
-}
-
-
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -35,7 +8,6 @@ if($id) {
     <title>Document</title>
 </head>
 <body>
-    <h1><?php echo "$scripture[book] $scripture[chapter]:$scripture[verse]";?></h1>
-    <p>"<?php echo $scripture['content'];?>"</p>
+    
 </body>
 </html>
