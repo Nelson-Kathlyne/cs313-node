17277453fcc2f8ad41c9ddf33d3e55546c7d6a9a
Kat Nelson <kat98058@byui.edu>
2021-02-13 21:09:32 -0600

    a

--- web/application/index.php
+++ web/application/index.php
@@ -13,7 +13,15 @@ if($search) {
 
 $stmt = $db->prepare($sql);
 
+$stmt->bindValue(':search', $search, PDO::PARAM_STR);
+$stmt->execute();
+$response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt->closeCursor();
 
+$message = "";
+if($stmt->rowCount() == 0) {
+  $message = "No results found.";
+}
 ?>
 <!DOCTYPE html>
 <html>
