20e8dbb91d79a278245f47eae40f7894a04d891f
Kat Nelson <kat98058@byui.edu>
2021-02-13 21:24:48 -0600

    a

--- web/application/index.php
+++ web/application/index.php
@@ -11,17 +11,17 @@ if($search) {
   $sql = "SELECT * from recipes WHERE recipeName = :search";
 }
 
-$stmt = $db->prepare($sql);
+ $stmt = $db->prepare($sql);
 
-$stmt->bindValue(':search', $search, PDO::PARAM_STR);
-$stmt->execute();
-$response = $stmt->fetchAll(PDO::FETCH_ASSOC);
-$stmt->closeCursor();
+// $stmt->bindValue(':search', $search, PDO::PARAM_STR);
+// $stmt->execute();
+// $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// $stmt->closeCursor();
 
-$message = "";
-if($stmt->rowCount() == 0) {
-  $message = "No results found.";
-}
+// $message = "";
+// if($stmt->rowCount() == 0) {
+//   $message = "No results found.";
+// }
 ?>
 <!DOCTYPE html>
 <html>
