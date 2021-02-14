cce96475bd7a80fe3a2ce62692e7a1e0a2f9759f
Kat Nelson <kat98058@byui.edu>
2021-02-13 21:41:19 -0600

    a

--- web/application/index.php
+++ web/application/index.php
@@ -1,9 +1,13 @@
 
 <?php
+require 'connection.php';
 
+$search = "";
+if(isset($_GET['search'])){
+  $search = filter_input(INPUT_GET,'search',FILTER_SANITIZE_STRING);
+}
 
-require "connection.php";
-$db = connectRecipeDb();
+$connection = connectRecipeDb();
 
 $sql = 'SELECT * from recipes';
 
@@ -11,17 +15,17 @@ if($search) {
   $sql = "SELECT * from recipes WHERE recipeName = :search";
 }
 
- $stmt = $db->prepare($sql);
+$stmt = $connection->prepare($sql);
 
-// $stmt->bindValue(':search', $search, PDO::PARAM_STR);
- $stmt->execute();
-// $response = $stmt->fetchAll(PDO::FETCH_ASSOC);
-// $stmt->closeCursor();
+$stmt->bindValue(':search', $search, PDO::PARAM_STR);
+$stmt->execute();
+$response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt->closeCursor();
 
-// $message = "";
-// if($stmt->rowCount() == 0) {
-//   $message = "No results found.";
-// }
+$message = "";
+if($stmt->rowCount() == 0) {
+  $message = "No results found.";
+}
 ?>
 <!DOCTYPE html>
 <html>
