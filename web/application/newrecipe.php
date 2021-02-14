151e586c68a4e6074b6fa140387b3e4e09dabf5c
Kat Nelson <kat98058@byui.edu>
2021-02-13 21:57:25 -0600

    a

---
+++ web/application/newrecipe.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Family Recipes</title>
+</head>
+
+<body>
+<div>
+
+<h1>Family Recipes</h1>
+
+<form action="/" method="set">
+    <label for="newRecipe"></label>
+    <input type="text" name="recipeName" id="recipeName">
+    <input type="text" name="recipeIngredients" id="recipeIngredients">
+    <input type="text" name="recipeInstructions " id="recipeInstructions">
+    <input type="submit" value="submit"/>
+  </form>
+ 
+
+</div>
+
+</body>
+</html>
+
