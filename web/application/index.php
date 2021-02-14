6df1c0a6a444bc6583129d1e1c6600691b5edef1
Kat Nelson <kat98058@byui.edu>
2021-02-13 20:35:12 -0600

    a

--- web/application/index.php
+++ web/application/index.php
@@ -17,31 +17,7 @@ $db = connectRecipeDb();
 
 <h1>Family Recipes</h1>
 
-<?php
 
-
-
-
-
-//  prepare the statement
-
-$sql = "SELECT recipeName, recipeIngredients, recipeInstructions FROM recipes";
-$stmt = $db ->prepare($sql);
-$statement->execute();
-
-// Go through each result
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-
-	$name = $row['recipeName'];
-	$ingredients = $row['recipeIngredients'];
-	$instructions = $row['recipeInstructions'];
-
-
-	echo "<p><h3>$name</h3><br>$ingredients<br>$instructions<p>";
-}
-
-?>
 
 
 </div>
