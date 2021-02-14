8c3a33db7f24cccf50e79deb94d67d5a7abb7ed6
Kat Nelson <kat98058@byui.edu>
2021-02-13 21:03:22 -0600

    a

--- web/application/index.php
+++ web/application/index.php
@@ -5,6 +5,23 @@
 require "connection.php";
 $db = connectRecipeDb();
 
+$sql = 'SELECT * from recipes';
+
+if($search) {
+  $sql = "SELECT * from recipes WHERE recipeName = :search";
+}
+
+$stmt = $connection->prepare($sql);
+
+$stmt->bindValue(':search', $search, PDO::PARAM_STR);
+$stmt->execute();
+$response = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$stmt->closeCursor();
+
+$message = "";
+if($stmt->rowCount() == 0) {
+  $message = "No results found.";
+}
 ?>
 <!DOCTYPE html>
 <html>
