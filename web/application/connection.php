49888a6dd8d8b20d841328f6c5e302e76c49e1d2
Kat Nelson <kat98058@byui.edu>
2021-02-18 21:39:47 -0600

    a

--- web/application/connection.php
+++ web/application/connection.php
@@ -1,9 +1,10 @@
-<?php 
+<?php function connectRecipeDb() {
+	$db = NULL;
 
-function connectRecipeDb() {
+	try {
 		$dbUrl = getenv('DATABASE_URL');
 
-		if ( empty($dbUrl)) {
+		if (!isset($dbUrl) || empty($dbUrl)) {
 
 			$dbUrl = "postgres://xazedpmayqitvl:5eec8f701e6172d54b89538d3c00881800b5e33eb0e178103feeec094250f9d3@ec2-52-207-124-89.compute-1.amazonaws.com:5432/d4llj7787ln62m";
 
@@ -18,18 +19,20 @@ function connectRecipeDb() {
 		$dbPassword = $dbopts["pass"];
 		$dbName = ltrim($dbopts["path"],'/');
 
-
-		$dsn = "pssql:host=$dbHost;port=$dbPort;dbname=$dbName";
-		$options = array (PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 		// Create the PDO connection
-		try{
-			$link = new PDO($dsn, $dbUser, $dbPassword, $options);
-			if(is_object($link)) {
-				return $link;
-			}
-		}
-	catch (PDOException $ex) {
-		var_dump ($ex);
-		exit;
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 	}
+	catch (PDOException $ex) {
+		
+		echo "Error connecting to DB. Details: $ex";
+	   exit;
+	}
+
+	return $db;
 }
+
+
+
+?>
