82f13cd1a339dd71eed551a66d032caf2e82712b
Nelson-Kathlyne <31799819+Nelson-Kathlyne@users.noreply.github.com>
2021-03-06 23:56:18 -0600

    Delete web/main directory

--- web/main/common/nav.php
+++
@@ -1,6 +0,0 @@
-<ul class = "menu">
-                    <li class = "menu_item"><a href="">Home</a></li>
-                    <li class = "menu_item"><a href="./views/aboutMe.php">About Me</a></li>
-                    <li class = "menu_item"><a href="./views/assignments.php">Class Work</a></li>
-           
-                </ul>
